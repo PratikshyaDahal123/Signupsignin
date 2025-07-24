d6c2141b4df3edf65e28d1a0e2e3349ad77dc2de
Pratikshya Dahal <pratikshyadahal92@gmail.com>
2025-07-24 21:06:48 +0545

    store items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Seating from './components/Seating';
-import TopBar from './components/TopBar';
 import Contact from './components/Contact';
 import AboutDecorish from './components/Aboutdecorish';
+
+import TopBar from './components/TopBar';
+import Navbar from './components/Navbar';
 
 import './App.css';
 
@@ -13,11 +16,12 @@ function App() {
     <Router>
       <>
         <TopBar />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/furniture/seating" element={<Seating />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<AboutDecorish />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </>
     </Router>
